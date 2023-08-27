dbad1df2f7744739858cdaa877b31e62e6650017
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-08-27 23:51:52 +0900

    追加图标功能

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,9 +5,15 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-
+import { mdi, aliases } from 'vuetify/iconsets/mdi'
 export default createVuetify({
     components,
     directives,
+    icons: {
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
 })
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <h1>
+      <Icon icon="material-symbols:11mp-sharp" />
+      <v-btn color="success"><v-icon>mdi-close</v-icon>text</v-btn>
+    </h1>
+    <v-icon icon="mdi-access-point-network" />
+    <img width="64" height="64" src="https://img.icons8.com/nolan/64/book.png" alt="book" />
+  </div>
 </template>
