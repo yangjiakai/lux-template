d3d1c0c4e8d67954e0b9f526066386899f199e15
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-08-27 10:36:38 +0900

    初始化

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+import vuetify from "@/plugins/vuetify";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import "@/styles/main.scss";
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(vuetify);
+app.mount('#app')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,84 @@
+<!--
+* @Component: 
+* @Maintainer: J.K. Yang
+* @Description: 
+-->
+<script setup lang="ts">
+const desserts = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237
+  },
+  {
+    name: 'Eclair',
+    calories: 262
+  },
+  {
+    name: 'Cupcake',
+    calories: 305
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375
+  },
+  {
+    name: 'Lollipop',
+    calories: 392
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408
+  },
+  {
+    name: 'Donut',
+    calories: 452
+  },
+  {
+    name: 'KitKat',
+    calories: 518
+  }
+]
+</script>
+
+<template>
+  <div class="about">
+    <!-- <div class="d-flex flex-wrap">
+      <v-card class="ma-2" v-for="i in 12" :key="i" :elevation="i" width="100" height="100">
+      </v-card>
+    </div> -->
+    <div>
+      <v-table>
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Calories</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in desserts" :key="item.name">
+            <td>{{ item.name }}</td>
+            <td>{{ item.calories }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
